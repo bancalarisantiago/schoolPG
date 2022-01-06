ca9458e883f378879923c07e8554eeebe896b9c1
Santiago Bancalari <bancalarisantiago@gmail.com>
2022-01-06 11:54:45 -0300

    set_up(api/client) : add create app ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -19,7 +19,8 @@ const PORT = process.env.PORT || 5000; // aca seteamos el puerto en el que va a 
 const uri ="mongodb+srv://ischool:escuelita420@cluster0.pfkbt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 
-mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology: true}) // conexion a la base de datos
-.then(() => app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))) // escucha en el puerto
-.catch((err) => console.log(err)); // error en la conexion
+mongoose
+    .connect(uri,{useNewUrlParser: true, useUnifiedTopology: true}) // conexion a la base de datos
+    .then(() => app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))) // escucha en el puerto
+    .catch((err) => console.log(err)); // error en la conexion
 
