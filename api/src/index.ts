44a114e4de3137c02c70ccd5c44cb595929197ee
usuario1 <danumillonaria44@gmail.com>
2022-01-07 19:02:09 -0300

    feat(api/morgan add):instale morgan

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 
 import router from "./routes/index";
 
@@ -8,8 +9,8 @@ const app = express();
 app.use(express.json({ limit: "30mb" })); // support json encoded bodies
 app.use(express.urlencoded({ limit: "30mb", extended: true })); // support encoded bodies
 app.use(cors()); // cors middleware sirve para permitir peticiones de otros dominios
-
-app.use("/posts", router);
+app.use(morgan('dev'))
+app.use("/api", router);
 // https://www.mongodb.com/cloud/atlas
 
 export default app;
