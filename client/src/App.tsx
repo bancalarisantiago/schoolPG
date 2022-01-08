c5e40c11be1f68595b6db6e0f7924892168e52a6
IlCabezon <aguscastro2014.ac@gmail.com>
2022-01-08 09:11:46 -0300

    feat(src/formLoginDesktop):Formulario de pnatalla de login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router";
 
 //pages
 //import Login from "./pages/Login";
+import FormLogin from "./components/Desktop/FormLogin/FormLogin";
 
 function App() {
   return (
     <div>
+      <FormLogin />
       {/* <Routes>
         <Route path="/" element={<Login />} />
       </Routes> */}
--- client/src/components/BgImage/index.tsx
+++
@@ -1,5 +0,0 @@
-import styles from "./BgImage.module.css";
-
-export default function BgImage() {
-  return <div className={styles.main}></div>;
-}
