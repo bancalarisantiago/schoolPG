394cbcfcb39823a2bb9b52a277731a2797bde9ac
usuario1 <danumillonaria44@gmail.com>
2022-01-08 15:03:13 -0300

    feact(api/controllers/admin/add):add router

--- api/src/routes/admin.ts
+++ api/src/routes/admin.ts
@@ -1,9 +1,9 @@
 import express from "express";
-import { getAdmins, createAdmin } from "../controllers/admin";
+import { getAdmins, createAdmin,addSchool } from "../controllers/admin";
 
 const router = express.Router();
 
 router.get('/admin', getAdmins);
 router.post('/admin', createAdmin);
-
+router.post('/admin/add', addSchool);
 export default router;
