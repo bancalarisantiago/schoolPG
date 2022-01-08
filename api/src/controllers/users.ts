6948d08296e26c85f3ab281caa37afc56bcbc81e
Santiago Bancalari <bancalarisantiago@gmail.com>
2022-01-08 18:33:07 -0300

    feat(api/route_user) add: fix and get users route

--- api/src/controllers/users.ts
+++ api/src/controllers/users.ts
@@ -4,49 +4,11 @@ import axios from "axios";
 //models
 import User from "../models/User/User";
 
-// https://www.restapitutorial.com/httpstatuscodes.html ---> status codes
-
-const getUsersApi = async function () {
-  try {
-    const user = await axios.get("https://randomuser.me/api/?results=10");
-
-    return user.data.results;
-  } catch {
-    return "Error de busqueda";
-  }
-};
-
-// export const getPosts = async (req:any, res:any) => {
-//   try{
-//     const postMessages = await PersonModel.find();
-
-//         const users = await getUsers();
-//     res.status(200).json(users);
-//   } catch (error: any) {
-//     res.status(404).json({message: error.message});
-//   }
-// }
 
 export const getUsers = async (req: any, res: any) => {
   try {
-    const postMessages = await User.find();
-
-    const users = await getUsersApi();
-
-    users.map((e: any) =>
-      User.create({
-        name: { first: e.name.first, last: e.name.last },
-        gender: e.gender,
-        location: {
-          number: e.location.street.number,
-          streetName: e.location.street.name,
-          locality: e.location.state,
-          postalCode: String(e.location.postcode),
-        },
-      })
-    );
-
-    res.status(200).json(users);
+    const allUsers= await User.find({});
+    res.status(200).json(allUsers);
   } catch (error: any) {
     res.status(404).json({ message: error.message });
   }
