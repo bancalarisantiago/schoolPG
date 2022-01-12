9fde18467ac76b347693c9c2c7f834ed53e6a933
IlCabezon <aguscastro2014.ac@gmail.com>
2022-01-12 13:36:37 -0300

    feat(client/radyForReduxUses)

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,12 +8,16 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 //redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
