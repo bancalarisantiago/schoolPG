853e7b1c39e66fba3bda82415c16dde59ee292ec
IlCabezon <aguscastro2014.ac@gmail.com>
2022-01-12 13:24:22 -0300

    Solving cors error

--- api/src/index.ts
+++ api/src/index.ts
@@ -6,9 +6,15 @@ import router from "./routes/index";
 
 const app = express();
 
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
 app.use(express.json({ limit: "30mb" })); // support json encoded bodies
 app.use(express.urlencoded({ limit: "30mb", extended: true })); // support encoded bodies
-app.use(cors()); // cors middleware sirve para permitir peticiones de otros dominios
+app.use(cors(corsOptions)); // cors middleware sirve para permitir peticiones de otros dominios
 app.use(morgan("dev"));
 app.use("/api", router);
 // https://www.mongodb.com/cloud/atlas
