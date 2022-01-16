64da1532e20fa5e82ef26c601b63a74fc61c2a84
IlCabezon <aguscastro2014.ac@gmail.com>
2022-01-16 16:17:19 -0300

    feat(client:componentAddSchool):ready

--- client/src/pages/Desktop/CreateSchool/CreateSchool.tsx
+++ client/src/pages/Desktop/CreateSchool/CreateSchool.tsx
@@ -1,174 +1,138 @@
 //components
-import Input from "../../../components/Desktop/ReusableComponents/Input/Input";
 import Button from "../../../components/Desktop/ReusableComponents/Button/Button";
 
-import { Link } from "react-router-dom";
-import useHelper from "./useHelper"
+import useHelper from "./useHelper";
 import styles from "./CreateSchool.module.css";
 
-
-
 const CreateSchool: React.FC = () => {
-   
-  const {
-    handleChange,
-    handleInputChange,
-    handleSubmit,
-    input,
-    location
-} = useHelper()
+  const { handleChange, handleInputChange, handleSubmit, input, location } =
+    useHelper();
 
-    return (
-        <div className={styles.main}>
-            <form onSubmit={handleSubmit}>
-                <Link to="/">
-                    Volver
-                </Link>
-                <h2>WELCOME</h2>
-                <div>
-                    <label className={styles.inputNames} htmlFor="name">
-                        Nombre:{" "}
-                    </label>
-                    <input
-                        type="text"
-                        placeholder="name"
-                        autoFocus
-                        name="name"
-                        id="name"
-                        value={input.name}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div>
-                    <label className={styles.inputNames} htmlFor="number">
-                        Number:{" "}
-                    </label>
-                    <input
-                        type="number"
-                        placeholder="numero"
-                        name="number"
-                        id="number"
-                        value={location.number}
-                        onChange={handleChange}
-                    />
-                    <label className={styles.inputNames} htmlFor="streeName">
-                        StreeName:{" "}
-                    </label>
-                    <input
-                        type="text"
-                        placeholder="streetName"
-                        name="streetName"
-                        id="streetName"
-                        value={location.streetName}
-                        onChange={handleChange}
-                    />
-                    <label className={styles.inputNames} htmlFor="postal">
-                        postal:{" "}
-                    </label>
-                    <input
-                        type="text"
-                        placeholder="codigo postal"
-                        name="postalCode"
-                        id="postalCode"
-                        value={location.postalCode}
-                        onChange={handleChange}
-                    />
-                    <label className={styles.inputNames} htmlFor="locality">
-                        Locality:{" "}
-                    </label>
-                    <input
-                        type="text"
-                        placeholder="locality"
-                        name="locality"
-                        id="locality"
-                        value={location.locality}
-                        onChange={handleChange}
-                    />
+  return (
+    <div className={styles.main}>
+      <div className={styles.formDiv}>
+        <div className={styles.form}>
+          <form onSubmit={handleSubmit} className={styles.formHtml}>
+            <div className={styles.header}>
+              <p className={styles.welcome}>Bienvenido a SchoolPg</p>
+            </div>
+            <div className={styles.inputsDiv}>
+              <div className={styles.inputC}>
+                <input
+                  className={styles.inputTag}
+                  autoComplete="off"
+                  placeholder="Nombre"
+                  name="name"
+                  value={input.name}
+                  onChange={handleInputChange}
+                />
+                <input
+                  className={styles.inputTag}
+                  autoComplete="off"
+                  placeholder="Numeracion"
+                  name="number"
+                  value={location.number}
+                  onChange={handleChange}
+                />
+              </div>
 
-                </div>
-                <div>
-                    <label className={styles.inputNames} htmlFor="orientation">
-                        Orientation:{" "}
-                    </label>
-                    <textarea
-                        placeholder="orientation"
-                        name="orientation"
-                        id="orientation"
-                        value={input.orientation}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div>
-                    <label className={styles.inputNames} htmlFor=" description">
-                        Description:{" "}
-                    </label>
-                    <textarea
-                        placeholder="description"
-                        name="description"
-                        id="description"
-                        value={input.description}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div>
-                    <label className={styles.inputNames} htmlFor="email">
-                        Email:{" "}
-                    </label>
-                    <input
-                        type="text"
-                        placeholder="email"
-                        name="email"
-                        id="email"
-                        value={input.email}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div>
-                    <label className={styles.inputNames} htmlFor="phone">
-                        Phone:{" "}
-                    </label>
-                    <input
-                        type="text"
-                        placeholder="phone"
-                        name="phone"
-                        id="phone"
-                        value={input.phone}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div>
-                    <label className={styles.inputNames} htmlFor="cellphone">
-                        Cellphone:{" "}
-                    </label>
-                    <input
-                        type="text"
-                        placeholder="cellphone"
-                        name="cellphone"
-                        id="cellphone"
-                        value={input.cellphone}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div>
-                    <label className={styles.inputNames} htmlFor="logo">
-                        Logo:{" "}
-                    </label>
-                    <input
-                        type="text"
-                        placeholder="logo"
-                        name="logo"
-                        id="logo"
-                        value={input.logo}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <Button text={"CreateSchool"}/>
-            </form>
+              <div className={styles.inputC}>
+                <input
+                  className={styles.inputTag}
+                  autoComplete="off"
+                  placeholder="Calle"
+                  name="streetName"
+                  value={location.streetName}
+                  onChange={handleChange}
+                />
+
+                <input
+                  className={styles.inputTag}
+                  autoComplete="off"
+                  placeholder="Codigo Postal"
+                  name="postalCode"
+                  value={location.postalCode}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className={styles.inputC}>
+                <input
+                  className={styles.inputTag}
+                  autoComplete="off"
+                  placeholder="Localidad"
+                  name="locality"
+                  value={location.locality}
+                  onChange={handleChange}
+                />
+                <input
+                  className={styles.inputTag}
+                  autoComplete="off"
+                  placeholder="Correo Electronico"
+                  name="email"
+                  value={input.email}
+                  onChange={handleInputChange}
+                />
+              </div>
+
+              <div className={styles.inputC}>
+                <input
+                  className={styles.inputTag}
+                  autoComplete="off"
+                  placeholder="Numero Fijo de Contacto"
+                  name="phone"
+                  value={input.phone}
+                  onChange={handleInputChange}
+                />
+
+                <input
+                  className={styles.inputTag}
+                  autoComplete="off"
+                  placeholder="Numero Celular"
+                  name="cellphone"
+                  value={input.cellphone}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className={styles.inputC}>
+                <input
+                  className={styles.inputTag}
+                  autoComplete="off"
+                  type="text"
+                  placeholder="Logo"
+                  name="logo"
+                  value={input.logo}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className={styles.textareaC}>
+                <textarea
+                  className={styles.textareaTag}
+                  placeholder="Orientacion"
+                  name="orientation"
+                  value={input.orientation}
+                  onChange={handleInputChange}
+                />
+
+                <textarea
+                  className={styles.textareaTag}
+                  placeholder="Descripcion"
+                  name="description"
+                  value={input.description}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className={styles.butDiv}>
+                <Button text={"CreateSchool"} />
+              </div>
+            </div>
+          </form>
         </div>
-    );
-}
+      </div>
+      <div className={styles.previewDiv}>
+        <div className={styles.preview}></div>
+      </div>
+    </div>
+  );
+};
 
-export default CreateSchool
-
-
-
+export default CreateSchool;
