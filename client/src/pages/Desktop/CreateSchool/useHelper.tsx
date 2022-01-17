f2a973f0093adbf752213d4e0052a1450d8833c9
IlCabezon <aguscastro2014.ac@gmail.com>
2022-01-17 02:36:38 -0300

    feat(morebugs)

--- client/src/pages/Desktop/CreateSchool/useHelper.tsx
+++ client/src/pages/Desktop/CreateSchool/useHelper.tsx
@@ -18,9 +18,9 @@ const useHelper = () => {
   const school = useSelector((state: IState) => state.userSession);
   const navigate = useNavigate();
 
-  //   useEffect(() => {
-  //     if (!school.id) navigate("/panel/general");
-  //   }, []);
+  useEffect(() => {
+    if (!school.id) navigate("/panel/general");
+  }, []);
 
   const [location, setLocation] = useState<Location>({
     number: "",
@@ -47,44 +47,42 @@ const useHelper = () => {
     });
   };
 
-    const handleChange = (e: ChangeEvent) => {
-      setLocation({
-        ...location,
-        [e.target.name]: e.target.value,
-      });
-      setInput({ ...input, location });
-    };
-
+  const handleChange = (e: ChangeEvent) => {
+    setLocation({
+      ...location,
+      [e.target.name]: e.target.value,
+    });
+    setInput({ ...input, location });
+  };
 
   const handleSubmit = async (e: SubmitEvent) => {
-      e.preventDefault();
-      dispatch(createSchool(input));
-    };
-     // $ npm install @emailjs/browser --save
+    e.preventDefault();
+    dispatch(createSchool(input));
+  };
+  // $ npm install @emailjs/browser --save
 
-    // init("user_zMyhB0L8PQmWO0uaVJjQO")
+  // init("user_zMyhB0L8PQmWO0uaVJjQO")
 
-    // const handleSubmitMail = async (e: SubmitEvent) => {
-    //     e.preventDefault();
-    //         const form: any = {name: "NUEVO USUARIO ",
-    //         from_name: "I SCHOOL APP",
-    //     reply_to: "bancalarisantiago@gmail.com"};
-            
-    //    emailjs.send('service_zstwmji', "template_wutf4po", form )
-    //    .then((result) => {
-    //     console.log(result.text);
-    // }, (error) => {
-    //     console.log(error.text);
-    // });
-  
-      return {
-        handleChange,
-        handleInputChange,
-        handleSubmit,
-        input,
-        location,
-      };
+  // const handleSubmitMail = async (e: SubmitEvent) => {
+  //     e.preventDefault();
+  //         const form: any = {name: "NUEVO USUARIO ",
+  //         from_name: "I SCHOOL APP",
+  //     reply_to: "bancalarisantiago@gmail.com"};
+
+  //    emailjs.send('service_zstwmji', "template_wutf4po", form )
+  //    .then((result) => {
+  //     console.log(result.text);
+  // }, (error) => {
+  //     console.log(error.text);
+  // });
+
+  return {
+    handleChange,
+    handleInputChange,
+    handleSubmit,
+    input,
+    location,
+  };
 };
-
 
 export default useHelper;
