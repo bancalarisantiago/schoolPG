978cece59d0f56f95d1c1e9258537b1754da7b99
IlCabezon <aguscastro2014.ac@gmail.com>
2022-01-17 03:21:58 -0300

    eaea

--- api/src/controllers/cCourse.ts
+++ api/src/controllers/cCourse.ts
@@ -8,89 +8,93 @@ import { ICourse } from "../models/Course/ICourse";
 
 //models
 import Course from "../models/Course/Course";
-import Subject from "../models/Subject/Subject"
+import Subject from "../models/Subject/Subject";
+import School from "../models/School/School";
 
-export const createCourse = (req: Request, res: Response) => {
+export const createCourse = async (req: Request, res: Response) => {
+  const { name, shifts, students, teachers, subjects, schoolId }: ICourse =
+    req.body;
+  console.log(schoolId);
+  try {
+    const newCourse = new Course({
+      name,
+      shifts,
+      students,
+      teachers,
+      subjects,
+    });
+    newCourse.save();
 
-    const { name, shifts, students, teachers, subjects }: ICourse = req.body;
+    const school = await School.findByIdAndUpdate(new toId(schoolId), {
+      $push: {
+        courses: new toId(newCourse._id),
+      },
+    });
 
-        try {
-            const newCourse = new Course({
-                name, shifts, students, teachers, subjects
-            })
-            newCourse.save();
-            res.status(200).json(newCourse);
-            } catch (error: any) {
-        res.status(404).json({ message: error.message });
-  } 
-}
+    res.status(200).json(newCourse);
+  } catch (error: any) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 export const getCourses = async (req: Request, res: Response) => {
-    try {
-        const allCourses = await Course.find({});
-        res.status(200).json(allCourses);
-      } catch (error: any) {
-        res.status(404).json({ message: error.message });
-      }
-}
-
+  try {
+    const allCourses = await Course.find({});
+    res.status(200).json(allCourses);
+  } catch (error: any) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 export const getCourseById = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const populateQuery = [{path: "teachers", model: "User"},
-                          {path: "students", model: "User"},
-                          {path: "subjects", model: "Subject"}
-                        ]
+  const { id } = req.params;
+  const populateQuery = [
+    { path: "teachers", model: "User" },
+    { path: "students", model: "User" },
+    { path: "subjects", model: "Subject" },
+  ];
   try {
-      const course = await Course.findById(id).populate(populateQuery).lean();
-      
-      res.status(200).json(course);
-    } catch (error: any) {
-      res.status(404).json({ message: error.message });
-    }
-}
+    const course = await Course.findById(id).populate(populateQuery).lean();
+
+    res.status(200).json(course);
+  } catch (error: any) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 export const addSubjectToCourse = async (req: Request, res: Response) => {
-      const { courseId, subjectId } = req.params;
-   
-      try {
+  const { courseId, subjectId } = req.params;
 
-        const course = await Course.findByIdAndUpdate(new toId(courseId), {
-              $push: {
-                subjects: new toId(subjectId),
-              },
-            });
-        const subject = await Subject.findByIdAndUpdate(new toId(subjectId), {
-          $push: {
-            courses: new toId(courseId),
-          },
-        });
-          res.status(200).json({ message: "relation was created succesfully" })
-
-      } catch (error: any) {
-
-        res.status(404).json({ error: "relation wasn'\t created succesfully" });
-      }
-
-}
-
+  try {
+    const course = await Course.findByIdAndUpdate(new toId(courseId), {
+      $push: {
+        subjects: new toId(subjectId),
+      },
+    });
+    const subject = await Subject.findByIdAndUpdate(new toId(subjectId), {
+      $push: {
+        courses: new toId(courseId),
+      },
+    });
+    res.status(200).json({ message: "relation was created succesfully" });
+  } catch (error: any) {
+    res.status(404).json({ error: "relation wasn'\t created succesfully" });
+  }
+};
 
 export const attendanceUpdate = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const { classAttend } = req.body;
-   
-    
-        try {
-          const course = await Course.findByIdAndUpdate(id,
-            {
-              $push: {
-                classes: classAttend
-              }
-            })
-            
-            res.status(200).json(course)
-        } catch(error) {
-          res.status(404).json(error)
-        }
+  const { id } = req.params;
+  const { classAttend } = req.body;
 
-}
+  try {
+    const course = await Course.findByIdAndUpdate(id, {
+      $push: {
+        classes: classAttend,
+      },
+    });
+
+    res.status(200).json(course);
+  } catch (error) {
+    res.status(404).json(error);
+  }
+};
