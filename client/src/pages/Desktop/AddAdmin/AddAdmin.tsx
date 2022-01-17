294c0b16e94e6e1970c67beebb25a0efb4b80d6b
anastasiotomas@gmail.com <anastasiotomas@gmail.com>
2022-01-17 02:22:19 -0300

    Co-authored-by: CorregidorOscar <CorregidorOscar@users.noreply.github.com> Co-authored-by: Santiago Bancalari <bancalarisantiago@users.noreply.github.com> Co-authored-by: MatiasFilliez <MatiasFilliez@users.noreply.github.com>

--- client/src/pages/Desktop/AddAdmin/AddAdmin.tsx
+++ client/src/pages/Desktop/AddAdmin/AddAdmin.tsx
@@ -1,12 +1,66 @@
-//css
+import Button from "../../../components/Desktop/ReusableComponents/Button/Button";
+import Input from "../../../components/Desktop/ReusableComponents/Input/Input";
 import styles from "./AddAdmin.module.css";
 
-const AddAdmin: React.FC = () => {
+import useHelper from "./useHelper"
+
+const AddStudent: React.FC = () => {
+
+  const { handleInputChange ,handleNameChange, handleSubmit,  input, name } = useHelper();
+
   return (
-    <div>
-      <p>AddAdmin</p>
+    <div className={styles.container}>
+      <div className={styles.submain}>
+        <div className={styles.titlebox}>
+          <p className={styles.title}>Añadir Admin</p>
+        </div>
+        <form className={styles.inputs} onSubmit={handleSubmit}>
+          <Input 
+            name="first"
+            autoComplete="off"
+            placeholder="Nombre"
+            value={name.first}
+            onChange={handleNameChange}
+          />
+          <Input 
+            name="last"
+            autoComplete="off"
+            placeholder="Apellido"
+            value={name.last}
+            onChange={handleNameChange}
+          />
+          <Input 
+            name="document"
+            autoComplete="off"
+            placeholder="Documento"
+            value={input.document}
+            onChange={handleInputChange}
+          />
+          <Input 
+            name="username"
+            autoComplete="off"
+            placeholder="Usuario"
+            value={input.username}
+            onChange={handleInputChange}
+          />
+          <Input 
+            name="password"
+            autoComplete="off"
+            placeholder="Contraseña"
+            value={input.document}
+            onChange={handleInputChange} />
+          <Input 
+            name="email"
+            autoComplete="off"
+            placeholder="E-mail"
+            value={input.email}
+            onChange={handleInputChange}  
+            />
+          <Button text="Añadir Admin" />
+        </form>
+      </div>
     </div>
   );
 };
 
-export default AddAdmin;
+export default AddStudent;
