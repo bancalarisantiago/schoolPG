3882116b116467c9a00bc1a4e2ae8fc36f4643bd
anastasiotomas@gmail.com <anastasiotomas@gmail.com>
2022-01-19 22:56:18 -0300

    feat(modularizadoDeRutasApp)

---
+++ client/src/routsApp.tsx
@@ -0,0 +1,42 @@
+import Login from "./pages/Desktop/Login/Login";
+import Panel from "./pages/Desktop/Panel/Panel";
+import CreateSchool from "./pages/Desktop/CreateSchool/CreateSchool";
+import AddStudent from "./pages/Desktop/AddStudent/AddStudent";
+import UpdateStudent from "./pages/Desktop/UpdateStudent/UpdateStudent";
+import AddTeacher from "./pages/Desktop/AddTeacher/AddTeacher";
+import UpdateTeacher from "./pages/Desktop/UpdateTeacher/UpdateTeacher";
+import AddAdmin from "./pages/Desktop/AddAdmin/AddAdmin";
+import UpdateAdmin from "./pages/Desktop/UpdateAdmin/UpdateAdmin";
+import AddCourse from "./pages/Desktop/AddCourse/AddCourse";
+import UpdateCourse from "./pages/Desktop/UpdateCourse/UpdateCourse";
+import AddEvent from "./pages/Desktop/AddEvent/AddEvent";
+import UpdateEvent from "./pages/Desktop/UpdateEvent/UpdateEvent";
+import AddSubject from "./pages/Desktop/AddSubject/AddSubject";
+import UpdateSubject from "./pages/Desktop/UpdateSubject/UpdateSubject";
+import SchoolInfo from "./pages/Desktop/SchoolInfo/SchoolInfo";
+import Error from "./pages/Desktop/Error/Error";
+import Profile from "./pages/Desktop/Profile/Profile";
+import UserDetail from "./pages/Desktop/UserDetail/UserDetail";
+
+
+export {
+    Login,
+    Panel,
+    CreateSchool,
+    AddStudent,
+    UpdateStudent,
+    AddTeacher,
+    UpdateTeacher,
+    AddAdmin,
+    UpdateAdmin,
+    AddCourse,
+    UpdateCourse,
+    AddEvent,
+    UpdateEvent,
+    AddSubject,
+    UpdateSubject,
+    SchoolInfo,
+    Error,
+    Profile,
+    UserDetail,
+};
