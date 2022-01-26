a6f83118ed07d713b0c3b69cad874fc6453d9457
Santiago Bancalari <bancalarisantiago@gmail.com>
2022-01-25 22:32:16 -0300

    feat(client/calendar_functions_style

--- client/src/components/Desktop/Calendar/Calendar.tsx
+++ client/src/components/Desktop/Calendar/Calendar.tsx
@@ -6,31 +6,71 @@ import interactionPlugin from "@fullcalendar/interaction";
 import FormEvent from "../FormEvent/FormEvent";
 import axios from "axios";
 //import moment from "moment"
-
+import {useState, useEffect} from "react"
 //Components
 import Button from "../ReusableComponents/Button/Button"
 //Styles
 import styles from "./Calendar.module.css"
 
+
 const Calendar: React.FC = () => {
   
-  const { calendarRef, eventsDb, setModalOpen, modalOpen, events, onEventAdded } = useHelper();
+const { calendarRef, eventsDb, setModalOpen, modalOpen, events, onEventAdded } = useHelper();
+
+
+const [ option, setOption ] = useState("dayGridMonth");
+
+
+useEffect(() => {
+ 
+},[option])
+
+function handleOptionChange (event: any) {
+  setOption(event.target.value)
+ event.target.value = "default"
+}
+
   
   return (
-    <>
+    < div className={styles.wrapper}>
     <section>
       
       <div >
         <div className={styles.calendar}>
-          <FullCalendar
-            ref={calendarRef}
-            aspectRatio={1.2}
-            plugins={[dayGridPlugin, interactionPlugin, listPlugin]}
-            initialView='listWeek'
-            events={eventsDb}
-            locale="es"
-            themeSystem="bootstrap"
-          />
+          {option !== "listWeek" && option !== "dayGridWeek" ?
+        
+              <FullCalendar
+                ref={calendarRef}
+                aspectRatio={2}
+                plugins={[dayGridPlugin, interactionPlugin, listPlugin]}
+                events={eventsDb}
+                initialView={`${option}`}
+                locale="es"
+                themeSystem="bootstrap"
+              />
+             : null }
+             
+             {option !== "dayGridMonth" &&  option !== "listWeek" ? 
+                  <FullCalendar
+                  ref={calendarRef}
+                  aspectRatio={2}
+                  plugins={[dayGridPlugin, interactionPlugin, listPlugin]}
+                  events={eventsDb}
+                  initialView={`${option}`}
+                  locale="es"
+                  themeSystem="bootstrap"
+                />
+             : null} 
+             {option !== "dayGridMonth" &&  option !== "dayGridWeek" ? 
+             <FullCalendar
+                ref={calendarRef}
+                aspectRatio={2}
+                plugins={[dayGridPlugin, interactionPlugin, listPlugin]}
+                events={eventsDb}
+                initialView={`${option}`}
+                locale="es"
+                themeSystem="bootstrap"
+              /> : null}
         </div>
       </div>
       <FormEvent
@@ -40,17 +80,22 @@ const Calendar: React.FC = () => {
         eventCreated={events}
       />
       
-    </section>
-        <div>
-          <div>
-              <img src={"https://upload.wikimedia.org/wikipedia/en/b/bc/Weather_on_iOS.png"}/>
-          </div> 
+    </section>  
+          <div className={styles.buttonsContainer}>
+                <div className={styles.select}>
+                  <select defaultValue="default" onChange={handleOptionChange}>
+                    <option value="default" disabled>Seleccionar Vista</option>
+                    <option value="dayGridMonth">Mes</option>
+                    <option value="listWeek">Dia</option>
+                    <option value="dayGridWeek">Semana</option>
+                  </select>
+                  </div>  
           <Button 
           text="Agregar Evento"
           onClick={() => setModalOpen(true)}/>
         </div>
-    </>
+    </div>
   );
 };
 
-export default Calendar;
+export default Calendar
