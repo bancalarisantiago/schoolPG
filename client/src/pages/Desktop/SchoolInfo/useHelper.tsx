84daddb1b62488904565f7ea699f6aca82239a0e
IlCabezon <aguscastro2014.ac@gmail.com>
2022-02-02 11:04:51 -0300

    feat(client/weather)

--- client/src/pages/Desktop/SchoolInfo/useHelper.tsx
+++ client/src/pages/Desktop/SchoolInfo/useHelper.tsx
@@ -1,10 +1,17 @@
 //from modules
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 //types
 import { IState } from "../../../interfaces";
 import { deleteCourseById, getSchoolById } from "../../../redux/actions";
+//assets
+import clearSkyIcon from "../../../assets/weatherIcon/clearSky.png";
+import rainy from "../../../assets/weatherIcon/raining.png";
+import thunderstorm from "../../../assets/weatherIcon/storm.png";
+import snoW from "../../../assets/weatherIcon/snow.png";
+import cloudy from "../../../assets/weatherIcon/cloudy.png";
+import mist from "../../../assets/weatherIcon/fog.png";
 
 const useHelper = () => {
   const userSession = useSelector((state: IState) => state.userSession);
@@ -15,14 +22,15 @@ const useHelper = () => {
       ? school.admins.length + school.students.length + school.teachers.length
       : " curso no cargado";
   };
+  const [weather, setWeather] = useState<any>();
 
   useEffect(() => {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(async (position) => {
         const { data } = await axios.get(
-          `https://api.openweathermap.org/data/2.5/onecall?lat=${position.coords.latitude}&lon=${position.coords.longitude}&exclude=alerts&appid=1903d69493c217289bc04bd041e51166`
+          `https://api.openweathermap.org/data/2.5/onecall?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=1903d69493c217289bc04bd041e51166&units=metric&lang=sp, es`
         );
-        console.log(data);
+        setWeather(data);
       });
     }
   }, []);
@@ -47,7 +55,84 @@ const useHelper = () => {
     );
   }
 
-  return { school, totalUsers, deleteCourse };
+  const day = (fecha: any) =>
+    [
+      "domingo",
+      "lunes",
+      "martes",
+      "miércoles",
+      "jueves",
+      "viernes",
+      "sábado",
+      "domingo",
+    ][new Date(fecha).getDay()];
+
+  const number: any = (fecha: any) => new Date(fecha).getDay();
+
+  const date = (date: any) => {
+    const month = date.split("/");
+
+    const months: any = {
+      1: "enero",
+      2: "febrero",
+      3: "marzo",
+      4: "abril",
+      5: "mayo",
+      6: "junio",
+      7: "julio",
+      8: "agosto",
+      9: "septiembre",
+      10: "octubre",
+      11: "noviembre",
+      12: "diciembre",
+    };
+    return [month[0], months[month[1]], month[2]];
+  };
+
+  const random = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  const clearSkyNight = ["clearSkyNight", "clearSkyNight2", "clearSkyNight3"];
+
+  const bg = (main: string) => {
+    const bg: any = {
+      "01d": ["clearSky", clearSkyIcon],
+      "01n": [clearSkyNight[random(0, 2)], clearSkyIcon],
+      "02d": ["fewClouds", cloudy],
+      "02n": ["fewCloudsNight", cloudy],
+      "03d": ["scatteredClouds", cloudy],
+      "03n": ["scatteredCloudsNight", cloudy],
+      "04d": ["brokenClouds", cloudy],
+      "04n": ["brokenCloudsNight", cloudy],
+      "09d": ["showerRain", rainy],
+      "09n": ["showerRainNight", rainy],
+      "10d": ["rain", rainy],
+      "10n": ["rainNight", rainy],
+      "11d": ["thunderstorm", thunderstorm],
+      "11n": ["thunderstormNight", thunderstorm],
+      "13d": ["snow", snoW],
+      "13n": ["snowNight", snoW],
+      "50d": ["mist"],
+      "50n": ["mistNight", mist],
+    };
+    return bg[main];
+  };
+
+  const dayByNum = (i: number) =>
+    ["dom", "lun", "mar", "mié", "jue", "vie", "sáb", "dom"][i];
+
+  return {
+    school,
+    weather,
+    bg,
+    day,
+    number,
+    dayByNum,
+    date,
+    totalUsers,
+    deleteCourse,
+  };
 };
 
 export default useHelper;
